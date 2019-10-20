206763d76d808f3329f79f0ca5dc15cd336adda0
Gabriel Silva <gsilva2510@hotmail.com>
2019-10-20 17:37:23 -0300

    angular updated to 8.2.11

--- src/app/ categories/category/categories.service.ts
+++ src/app/ categories/category/categories.service.ts
@@ -9,7 +9,6 @@ import { Observable } from 'rxjs';
 import { MEAT_API } from '../../app.api';
 
 import { category } from './category.model';
-import { ErrorHandler } from '../../app.error-handler';
 import { MenuItem } from '../../category-detail/menu-item/menu-item.model';
 
 
@@ -23,24 +22,24 @@ export class categoriesService {
   categories(search?: string): Observable<category[]> {
     return this.http.
       get(`${MEAT_API}/categories`, {params: {q: search}}).
-      map(response => response.json()).catch(ErrorHandler.handleError);
+      map(response => response.json());
   }
 
   categoryById(id: string): Observable<category> {
     return this.http.get(`${MEAT_API}/categories/${id}`).
-      map(response => response.json()).catch(ErrorHandler.handleError);
+      map(response => response.json());
   }
 
   reviewsOfcategory(id: string): Observable<any> {
     return this.http.
       get(`${MEAT_API}/categories/${id}/reviews`).
-      map(response => response.json()).catch(ErrorHandler.handleError);
+      map(response => response.json());
   }
 
   menuOfcategory(id: string): Observable<MenuItem[]> {
     return this.http.
       get(`${MEAT_API}/categories/${id}/menu`).
-      map(response => response.json()).catch(ErrorHandler.handleError);
+      map(response => response.json());
   }
   
 }
